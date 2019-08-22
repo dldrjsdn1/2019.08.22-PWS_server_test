758fdbe6766e1be8e90736b6863824c1fa9436d3
dldrjsdn1 <dldrjsdn1@naver.com>
2019-08-22 16:53:28 +0900

    test

--- scripts/app.js
+++ scripts/app.js
@@ -60,7 +60,7 @@
 
   // TODO: 아래에 서비스워커 등록
   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./PWS-server/service-worker.js').then(() => {console.log('Service Worker Registered');
+    navigator.serviceWorker.register('./service-worker.js').then(() => {console.log('Service Worker Registered');
   });
   }
 })();
